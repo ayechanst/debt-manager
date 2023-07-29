ccd7bfb6f2e04943c3d0e5e741925b25e1abafac
Aye Chan <ayechan.sant@gmail.com>
2023-07-28 17:38:44 -0700

    getting the checkbox to pass its value into setCheckbox data

--- packages/nextjs/components/AddPurchase.tsx
+++ packages/nextjs/components/AddPurchase.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 // import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+interface CheckboxData {
+  name: string;
+}
 
 export const AddPurchase = () => {
   // first chooseBeneficiaries
@@ -11,10 +15,27 @@ export const AddPurchase = () => {
   const [purchaseName, setPurchaseName] = useState("");
   const people: string[] = [];
 
-  // submits form
+  // checkBoxData is array
+  const [checkboxData, setCheckboxData] = useState<CheckboxData[]>([]);
+
+  function handleCheck(name: string) {
+    setCheckboxData(prevData => prevData.concat({ name }));
+  }
+
+  // submits form, both checkButtons and logPurchase
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
   }
+
+  // this is for the checkButton submition
+  const { submitChecks } = useScaffoldContractWrite({
+    contractName: "YourContract",
+    functionName: "chooseBeneficiaries",
+    args: [checkboxData],
+    onBlockConfirmation: txnReceipt => {
+      console.log("game created", txnReceipt.blockHash);
+    },
+  });
 
   const { data: peopleObject } = useScaffoldContractRead({
     contractName: "YourContract",
@@ -53,7 +74,17 @@ export const AddPurchase = () => {
             {listItems.map((name, index) => (
               <div key={index}>
                 <label className="flex">
-                  <input type="checkbox" className="checkbox" />
+                  {/* make this fill out formData depending on who is checked */}
+                  <input
+                    type="checkbox"
+                    className="checkbox"
+                    onChange={() => {
+                      handleCheck(name);
+                      {
+                        /* fix the fact we can pass name as an arg */
+                      }
+                    }}
+                  />
                   {name}
                 </label>
               </div>
